d9a35192abee8ae0c5dca50383c5c80932b9ce28
Snigdha Chathley <snigdhachathley@Snigdhas-MacBook-Air.local>
2024-08-01 16:24:17 -0500

    product seeder created

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -12,6 +12,21 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $products = [
+            ['sku' => 'CC-001', 'name' => 'Grey Sweater', 'description' => 'A stylish grey sweater.', 'price' => 59.99, 'stock_quantity' => 100],
+            ['sku' => 'CC-002', 'name' => 'Black Cardigan', 'description' => 'A trendy black cardigan.', 'price' => 79.99, 'stock_quantity' => 50],
+        ];
+
+        for ($i = 3; $i <= 50; $i++) {
+            $products[] = [
+                'sku' => 'CC-' . str_pad($i, 3, '0', STR_PAD_LEFT),
+                'name' => 'Product ' . $i,
+                'description' => 'Description for product ' . $i,
+                'price' => rand(10, 100) + 0.99,
+                'stock_quantity' => rand(10, 100)
+            ];
+        }
+
+        DB::table('products')->insert($products);
     }
 }
