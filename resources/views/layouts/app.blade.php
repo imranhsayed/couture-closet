7b7319773e76f8d84c6d330a9d4e3e93da5b538f
ParthKher01 <157850139+ParthKher01@users.noreply.github.com>
2024-08-01 10:31:30 -0500

    jquery dependency added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -163,6 +163,7 @@
     </div>
 
     <!-- Bootstrap JS and dependencies -->
+	<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 </body>
 <svg width="0" height="0" class="hidden">
