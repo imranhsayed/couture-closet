7d8c886653c99dad164aeb2295e89df9f3846524
Donald Obi <obidonald.o@gmail.com>
2024-08-02 15:10:54 -0500

    created cart layout

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,7 +1,85 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container">
-		<h1 class="text-center mb-4">Checkout</h1>
-	</div>
+<section class="hero py-6">
+    <div class="container">
+        <!-- Breadcrumbs -->
+        <ol class="breadcrumb ps-0 ">
+            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+            <li class="breadcrumb-item active">Checkout</li>
+        </ol>
+        <!-- Hero Content-->
+        <div class="hero-content">
+            <h1 class="hero-heading">Checkout</h1>
+            <div>
+                <p class="lead text-muted">Are you a returning customer? <a href="#" class="fw-bold">Login</a>.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="container pb-6">
+    <div class="row">
+        <div class="col-lg-7 pe-xl-6">
+            <form action="checkout-confirmed.html">
+                <h5 class="text-primary mb-5">Shipping address </h5>
+                <div class="row">
+                    <div class="mb-3 col-md-6 mb-4">
+                        <label class="form-label" for="fullname_invoice">Full Name</label>
+                        <input class="form-control form-control-underlined ps-0" type="text" name="fullname_invoice"
+                            id="fullname_invoice">
+                        <div data-lastpass-icon-root=""
+                            style="position: relative !important; height: 0px !important; width: 0px !important; float: left !important;">
+                        </div>
+                    </div>
+                    <div class="mb-3 col-md-6 mb-4">
+                        <label class="form-label" for="emailaddress_invoice">Email Address</label>
+                        <input class="form-control form-control-underlined ps-0" type="text" name="emailaddress_invoice"
+                            id="emailaddress_invoice">
+                    </div>
+                    <div class="mb-3 col-md-6 mb-4">
+                        <label class="form-label" for="street_invoice">Street</label>
+                        <input class="form-control form-control-underlined ps-0" type="text" name="street_invoice"
+                            id="street_invoice">
+                    </div>
+                    <div class="mb-3 col-md-6 mb-4">
+                        <label class="form-label" for="city_invoice">City</label>
+                        <input class="form-control form-control-underlined ps-0" type="text" name="city_invoice"
+                            id="city_invoice">
+                    </div>
+                    <div class="mb-3 col-md-6 mb-4">
+                        <label class="form-label" for="zip_invoice">ZIP</label>
+                        <input class="form-control form-control-underlined ps-0" type="text" name="zip_invoice"
+                            id="zip_invoice">
+                    </div>
+                    <div class="mb-3 col-md-6 mb-4">
+                        <label class="form-label" for="state_invoice">State</label>
+                        <input class="form-control form-control-underlined ps-0" type="text" name="state_invoice"
+                            id="state_invoice">
+                    </div>
+                    <div class="mb-3 col-md-6 mb-4">
+                        <label class="form-label" for="phonenumber_invoice">Phone Number</label>
+                        <input class="form-control form-control-underlined ps-0" type="text" name="phonenumber_invoice"
+                            id="phonenumber_invoice">
+                    </div>
+                    <div class="mb-3 col-12 mt-3">
+                        <div class="form-check">
+                            <input class="form-check-input" id="show-shipping-address" type="checkbox"
+                                name="show-shipping-address" data-bs-toggle="collapse"
+                                data-bs-target="#shippingAddress">
+                            <label class="form-check-label align-middle" for="show-shipping-address">Use a different
+                                billing address</label>
+                        </div>
+                    </div>
+                    <!-- /Shipping Address-->
+                </div>
+                <!-- Billing Address-->
+
+                <!-- /Billing Address -->
+            </form>
+        </div>
+        <div class="col-lg-5 summary">
+        </div>
+    </div>
+</div>
 @endsection
