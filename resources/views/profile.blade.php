46ea4ecc64a220cd333e5795d1fa873eb556d52d
ParthKher01 <157850139+ParthKher01@users.noreply.github.com>
2024-08-06 14:51:14 -0500

    Profile.blade.php updated

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,18 +3,57 @@
 @section('content')
 	<div class="container">
 		<h1 class="text-center mb-4">Profile</h1>
-		<div id="user-profile" class="card p-4">
-            <div class="row">
-                <div class="col-md-4">
-                    <img src="https://via.placeholder.com/150" alt="User Image" class="img-fluid rounded-circle mb-3">
+		<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <div class="card">
+                <div class="card-header">
+                    <h4>User Profile</h4>
                 </div>
-                <div class="col-md-8">
-                    <h4 id="user-name"><strong>First Name</strong></h4>
-                    <p id="user-email"><strong>Last Name</strong></p>
-                    <p id="user-address"><strong>Email</strong></p>
-                    <p id="user-phone"><strong>Address</strong></p>
+                <div class="card-body">
+                    <h5 class="mb-3">Personal Information</h5>
+                    <div class="form-group">
+                        <label>First Name:</label>
+                        <p>{{ $user->first_name }}</p>
+                    </div>
+                    <div class="form-group">
+                        <label>Last Name:</label>
+                        <p>{{ $user->last_name }}</p>
+                    </div>
+                    <div class="form-group">
+                        <label>Email:</label>
+                        <p>{{ $user->email }}</p>
+                    </div>
+                    <div class="form-group">
+                        <label>Phone:</label>
+                        <p>{{ $user->telephone }}</p>
+                    </div>
+
+                    <h5 class="mt-4 mb-3">Residential Information</h5>
+                    <div class="form-group">
+                        <label>House No:</label>
+                        <p>{{ $user->address->house_no }}</p>
+                    </div>
+                    <div class="form-group">
+                        <label>Street:</label>
+                        <p>{{ $user->address->street }}</p>
+                    </div>
+                    <div class="form-group">
+                        <label>City:</label>
+                        <p>{{ $user->address->city }}</p>
+                    </div>
+                    <div class="form-group">
+                        <label>Province:</label>
+                        <p>{{ $user->address->province }}</p>
+                    </div>
+                    <div class="form-group">
+                        <label>Country:</label>
+                        <p>{{ $user->address->country }}</p>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
 	</div>
 @endsection
