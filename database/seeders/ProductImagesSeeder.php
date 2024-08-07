feae3bbae5fa282e54c1d78f71be996f4a29cce0
ParthKher01 <157850139+ParthKher01@users.noreply.github.com>
2024-08-07 11:40:03 -0500

    Update productimagesseeder

--- database/seeders/ProductImagesSeeder.php
+++ database/seeders/ProductImagesSeeder.php
@@ -15,7 +15,7 @@ class ProductImagesSeeder extends Seeder
     {
         $productImages = [];
         $images = [
-            1 => ['grey_sweater_1.jpg'],
+            1 => ['grey_sweater_1.jpg', 'grey_sweater_2.jpg'],
             2 => ['black_cardigan_1.jpg'],
             3 => ['beige_cardigan_1.jpg', 'beige_cardigan_2.jpg'],
             4 => ['orange_cardigan_1.jpg', 'orange_cardigan_2.jpg'],
@@ -23,7 +23,7 @@ class ProductImagesSeeder extends Seeder
             6 => ['cream_jacket_1.jpg'],
             7 => ['yellow_jacket_1.jpg', 'yellow_jacket_2.jpg'],
             8 => ['pink_feather_jacket_1.jpg', 'pink_feather_jacket_2.jpg'],
-            9 => ['grey_jacket_1.jpg'],
+            9 => ['grey_jacket_1.jpg', 'grey_jacket_2.jpg'],
             10 => ['rust_cardigan_1.jpg'],
             11 => ['beige_dress_1.jpg'],
             12 => ['denim_jacket_1.jpg'],
