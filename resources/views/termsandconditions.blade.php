5bf88cd2dec60960c56e3e2a67eac11f439accb4
Snigdha Chathley <snigdhachathley@Snigdhas-MacBook-Air.local>
2024-08-08 10:57:55 -0500

    terms and conditions page content added

---
+++ resources/views/termsandconditions.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container my-5">
+    <h1>Terms and Conditions</h1>
+    <p>Welcome to Couture Closet! These terms and conditions outline the rules and regulations for the use of Couture Closet's Website, located at www.couturecloset.com.</p>
+
+    <h2>Introduction</h2>
+    <p>By accessing this website, we assume you accept these terms and conditions. Do not continue to use Couture Closet if you do not agree to take all of the terms and conditions stated on this page.</p>
+
+    <h2>License</h2>
+    <p>Unless otherwise stated, Couture Closet and/or its licensors own the intellectual property rights for all material on Couture Closet. All intellectual property rights are reserved. You may access this from Couture Closet for your own personal use subjected to restrictions set in these terms and conditions.</p>
+    <p>You must not:</p>
+    <ul>
+        <li>Republish material from Couture Closet</li>
+        <li>Sell, rent, or sub-license material from Couture Closet</li>
+        <li>Reproduce, duplicate, or copy material from Couture Closet</li>
+        <li>Redistribute content from Couture Closet</li>
+    </ul>
+
+    <h2>User Comments</h2>
+    <p>This Agreement shall begin on the date hereof. Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. Couture Closet does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of Couture Closet, its agents, and/or affiliates. Comments reflect the views and opinions of the person who posts their views and opinions.</p>
+
+    <h2>Hyperlinking to our Content</h2>
+    <p>The following organizations may link to our Website without prior written approval:</p>
+    <ul>
+        <li>Government agencies</li>
+        <li>Search engines</li>
+        <li>
