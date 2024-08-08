9fd820608698643c8af726f583816f77ffb5548a
Snigdha Chathley <snigdhachathley@Snigdhas-MacBook-Air.local>
2024-08-08 11:33:32 -0500

    terms and conditions page--- Complete

--- resources/views/termsandconditions.blade.php
+++ resources/views/termsandconditions.blade.php
@@ -50,4 +50,12 @@
     <p>To the maximum extent permitted by applicable law, we exclude all representations, warranties, and conditions relating to our website and the use of this website. Nothing in this disclaimer will:</p>
     <ul>
         <li>Limit or exclude our or your liability for death or personal injury;</li>
+        <li>Limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
+        <li>Limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
+        <li>Exclude any of our or your liabilities that may not be excluded under applicable law.</li>
+    </ul>
+    <p>The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and (b) govern         all liabilities arising under the disclaimer, including liabilities arising in contract, in tort, and for breach of statutory duty.</p>
+    <p>As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.</p>
+</div>
+@endsection
 
