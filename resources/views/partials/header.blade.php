ba1c50185f99e9c6fe26d1803332277180051175
ParthKher01 <157850139+ParthKher01@users.noreply.github.com>
2024-08-09 13:48:54 -0500

    Shop Slider updated -> Added Shop -> All Products

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,6 @@
 <div class="col-lg-4 col-sm-6 p-lg-5">
+	<h6 class="dropdown-header h6 ps-lg-0">Shop</h6>
+	<a class="dropdown-item ps-lg-0" href="/shop">All Products</a>
 	<h6 class="dropdown-header h6 ps-lg-0">Demography</h6>
 	@if(isset($categories))
 		@foreach ($categories as $category)
