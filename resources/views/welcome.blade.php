3f0e19a4d8aeefbbc1bf556fcf04922b3a603a35
ParthKher01 <157850139+ParthKher01@users.noreply.github.com>
2024-08-09 14:48:54 -0500

    Slider updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,35 +9,32 @@
 		</div>
 		<div class="carousel-inner">
 			<div class="carousel-item">
-				<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
-
+				<img src="/images/beige_cardigan_1.jpg" alt="First slide" class="d-block w-100" style="aspect-ratio: 4/3; height: 512px; object-fit: cover;">
 				<div class="container">
 					<div class="carousel-caption text-start">
 						<h1>Example headline.</h1>
 						<p>Some representative placeholder content for the first slide of the carousel.</p>
-						<p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+						<p><a class="btn btn-lg btn-primary" href="/login">Sign up today</a></p>
 					</div>
 				</div>
 			</div>
 			<div class="carousel-item">
-				<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
-
+			<img src="/images/beige_cardigan_1.jpg" alt="First slide" class="d-block w-100" style="aspect-ratio: 4/3; height: 512px; object-fit: cover;">
 				<div class="container">
 					<div class="carousel-caption">
 						<h1>Another example headline.</h1>
 						<p>Some representative placeholder content for the second slide of the carousel.</p>
-						<p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+						<p><a class="btn btn-lg btn-primary" href="/about">Learn more</a></p>
 					</div>
 				</div>
 			</div>
 			<div class="carousel-item active">
-				<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
-
+			<img src="/images/beige_cardigan_1.jpg" alt="First slide" class="d-block w-100" style="aspect-ratio: 4/3; height: 512px; object-fit: cover;">
 				<div class="container">
 					<div class="carousel-caption text-end">
 						<h1>One more for good measure.</h1>
 						<p>Some representative placeholder content for the third slide of this carousel.</p>
-						<p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+						<p><a class="btn btn-lg btn-primary" href="/shop">Browse gallery</a></p>
 					</div>
 				</div>
 			</div>
