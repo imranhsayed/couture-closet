5104261cbfe24f0ffe4357ef3b50a9b2fa1523d7
ParthKher01 <157850139+ParthKher01@users.noreply.github.com>
2024-08-10 13:10:47 -0500

    filter addded on navbar shop menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -14,7 +14,7 @@
 	<h6 class="dropdown-header h6 ps-lg-0">Brand</h6>
 	@if(isset($brands))
 		@foreach($brands as $brand)
-			<a class="dropdown-item ps-lg-0" href="/shop">{{ $brand->value}}</a>
+			<a class="dropdown-item ps-lg-0" href="/shop?brand={{ $brand->value }}">{{ $brand->value}}</a>
 		@endforeach
 	@else 
 		<p>No Brand found.</p>
