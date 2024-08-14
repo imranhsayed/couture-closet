7c63882e514f2a19cbb8de2224ab3f9749c1d9ab
Donald Obi <obidonald.o@gmail.com>
2024-08-14 17:16:49 -0500

    updated password input section

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -34,7 +34,20 @@
                                             </div>
                                         </div>
 
-
+                                        <div class="col-12">
+                                            <div class="input-style-1">
+                                                <label for="password">{{ __('Password') }}</label>
+                                                <input id="password" type="password"
+                                                    class="form-control form-control-lg @error('password') is-invalid @enderror"
+                                                    name="password" placeholder="Password" required
+                                                    autocomplete="new-password">
+                                                @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                                @enderror
+                                            </div>
+                                        </div>
 
                                         <div class="col-12">
                                             <div class="input-style-1">
