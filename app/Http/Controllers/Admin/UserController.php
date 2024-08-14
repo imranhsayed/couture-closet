066d8686830ea39e32eda2dc74f7da23c37d8fbb
Snigdha Chathley <snigdhachathley@Snigdhas-MacBook-Air.local>
2024-08-14 11:46:45 -0500

    USErcontroller function development

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -5,3 +5,18 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('admin.users.index', compact('users'));
+    }
+
+    public function create()
+    {
+        return view('admin.users.create');
+    }
+
+}
